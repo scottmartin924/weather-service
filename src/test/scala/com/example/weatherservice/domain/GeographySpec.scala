307c1933f7fd8f97be0be5c14103152c1df56c10
scott.martin <scottmartin924@gmail.com>
2023-03-13 11:50:10 -0500

    Formatting test fil

--- src/test/scala/com/example/weatherservice/domain/GeographySpec.scala
+++ src/test/scala/com/example/weatherservice/domain/GeographySpec.scala
@@ -4,12 +4,11 @@ import com.example.weatherservice.domain.geography.GeographicPoint
 import zio.test.Assertion.equalTo
 import zio.test._
 
-
 object GeographySpec extends ZIOSpecDefault {
   override def spec =
     suite("GeographyDomainSpec")(
       test("Can make valid geographic point with positive lat/long") {
-        val lat  = 37.4109
+        val lat = 37.4109
         val long = 94.7050
         val result = for {
           point <- GeographicPoint.make(lat, long)
@@ -19,9 +18,12 @@ object GeographySpec extends ZIOSpecDefault {
       test("Can make valid geographic point with negative lat/long") {
         val lat = -37.4109
         val long = -94.7050
-        val result = for {
-          point <- GeographicPoint.make(lat, long)
-        } yield assert(point.lat.value)(Assertion.equalTo(lat)) && assert(point.long.value)(equalTo(long))
+        val result =
+          for {
+            point <- GeographicPoint.make(lat, long)
+          } yield assert(point.lat.value)(Assertion.equalTo(lat)) && assert(point.long.value)(
+            equalTo(long),
+          )
         assertTrue(result.isRight)
       },
       test("Invalid longitude fails to make") {
