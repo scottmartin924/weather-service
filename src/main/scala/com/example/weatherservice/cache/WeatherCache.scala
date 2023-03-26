49c71af7e4441aa6c385a13f0e171a24fb6e6cf4
scott.martin <scottmartin924@gmail.com>
2023-03-26 10:14:54 -0500

    Change to immutable map for cache

--- src/main/scala/com/example/weatherservice/cache/WeatherCache.scala
+++ src/main/scala/com/example/weatherservice/cache/WeatherCache.scala
@@ -5,10 +5,8 @@ import com.example.weatherservice.domain.client.{WeatherGridPoint, WeatherReport
 import zio.{Clock, Ref, ZIO, ZLayer}
 
 import java.time.Instant
-import scala.collection.mutable
 
-// QUESTION: Why make this mutable? Think about it some
-class WeatherCache private (ref: Ref[mutable.Map[WeatherGridPoint, CacheValue[WeatherReport]]])
+class WeatherCache private (ref: Ref[Map[WeatherGridPoint, CacheValue[WeatherReport]]])
     extends Cache[WeatherGridPoint, WeatherReport] {
   override def get(key: WeatherGridPoint): ZIO[Any, Nothing, Option[WeatherReport]] = for {
     cache <- ref.get
@@ -30,8 +28,7 @@ class WeatherCache private (ref: Ref[mutable.Map[WeatherGridPoint, CacheValue[We
       expireTime: Instant = Instant.MAX,
     ): ZIO[Any, Nothing, Unit] =
     ref.update { cache =>
-      // NOTE: This only works b/c addOne returns entire map...which is nice
-      cache.addOne(key -> CacheValue(value, expireTime))
+      cache + (key -> CacheValue(value, expireTime))
     }
 
   override def clear(): ZIO[Any, Nothing, Unit] = ref.update(_.empty)
@@ -40,7 +37,7 @@ class WeatherCache private (ref: Ref[mutable.Map[WeatherGridPoint, CacheValue[We
 object WeatherCache {
   val inMemoryWeatherCache: ZLayer[Any, Nothing, WeatherCache] = ZLayer {
     Ref
-      .make(mutable.Map.empty[WeatherGridPoint, CacheValue[WeatherReport]])
+      .make(Map.empty[WeatherGridPoint, CacheValue[WeatherReport]])
       .map(new WeatherCache(_))
   }
 }
